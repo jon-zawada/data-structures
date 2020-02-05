7f0e11d652d6c34c6b382951ce13d0e6d8edeae0
jon-zawada <jonathan_zawada@yahoo.com>
2020-02-05 12:21:31 -0800

    Initial commit stack and quque working properly - very similar

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var numericKey = 0;
+  var startOfStor = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    //add the current value to storage by a numeric key
+    storage[numericKey] = value;
+    numericKey++;
   };
 
   someInstance.dequeue = function() {
+    //takes the first value in storage and returns it while
+    //deleting the first property as it has been 'served'
+    var currentlyServing = storage[startOfStor];
+    delete storage[startOfStor];
+    startOfStor++;
+    return currentlyServing;
   };
 
   someInstance.size = function() {
+    //Object.keys.length
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,24 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var numericKey = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    //puts value into storage by means of numeric key
+    storage[numericKey] = value;
+    numericKey++;
   };
 
   someInstance.pop = function() {
+    //'serves' the last value in storage and deletes it
+    var currentlyServing = storage[Object.keys(storage).length - 1];
+    delete storage[Object.keys(storage).length - 1];
+    numericKey--;
+    return currentlyServing;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
