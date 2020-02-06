ca02ab9a533bf85e3c31c6621a090adf85b9e846
jon-zawada <jonathan_zawada@yahoo.com>
2020-02-06 10:13:18 -0800

    Passing all es6 queue and stack

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,24 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.numericKey = 0;
+    this.startOfStor = 0;
   }
-
 }
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.numericKey] = value;
+  this.numericKey++;
+};
+
+Queue.prototype.dequeue = function() {
+  var currentlyServing = this.storage[this.startOfStor];
+  delete this.storage[this.startOfStor];
+  this.startOfStor++;
+  return currentlyServing;
+};
+
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,24 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.numericKey = 0;
   }
 
 }
+
+Stack.prototype.push = function(value) {
+  this.storage[this.numericKey] = value;
+  this.numericKey++;
+};
+
+Stack.prototype.pop = function() {
+  var currentlyServing = this.storage[this.numericKey - 1];
+  delete this.storage[this.numericKey - 1];
+  this.numericKey--;
+  return currentlyServing;
+};
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
