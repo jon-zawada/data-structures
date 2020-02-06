2dd2233251d7ed7611fce3dd411da4ff44bfafac
jon-zawada <jonathan_zawada@yahoo.com>
2020-02-05 21:14:07 -0800

    Functional-shared with neither of the removal methods working

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  numericKey = 0;
+  var someInstance = {};
+  someInstance.storage = {};
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.numericKey] = value;
+  this.numericKey++;
+};
 
+stackMethods.pop = function() {
+  var currentlyServing = Object.keys(this.storage.length - 1);
+  delete this.storage[Object.keys(this.storage.length - 1)];
+  this.numericKey--;
+  return currentlyServing;
+};
+
+stackMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
