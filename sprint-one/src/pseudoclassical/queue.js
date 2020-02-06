383d6d739d2e16b5bfc81e4a49315c6df5690a1f
jon-zawada <jonathan_zawada@yahoo.com>
2020-02-06 08:53:25 -0800

    Pseudoclassical stack and queue passing all tests

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,23 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.numericKey = 0;
+  this.startOfStor = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.numericKey] = value;
+  this.numericKey++;
+};
 
+Queue.prototype.dequeue = function() {
+  var currentlyServing = this.storage[this.startOfStor];
+  delete this.storage[this.startOfStor];
+  this.startOfStor++;
+  return currentlyServing;
+};
+
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,12 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var someInstance = new StackMethods();
-  return someInstance;
+  this.storage = {};
+  this.numericKey = 0;
 };
 
-var StackMethods = function() {};
 
-StackMethods.prototype.push = function() {
-  
+Stack.prototype.push = function(value) {
+  this.storage[this.numericKey] = value;
+  this.numericKey++;
+};
+
+Stack.prototype.pop = function() {
+  var currentlyServing = this.storage[this.numericKey - 1];
+  delete this.storage[this.numericKey - 1];
+  this.numericKey--;
+  return currentlyServing;
+};
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
 };
